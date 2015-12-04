018ec671e541f1bb64775f20a28c11e0851b2540
Travis Graham <travis@mobilecommons.com>
2015-12-04 01:40:07 -0500

    update for Google CSE

--- lib/lita/handlers/google_images.rb
+++ lib/lita/handlers/google_images.rb
@@ -3,18 +3,22 @@ require "lita"
 module Lita
   module Handlers
     class GoogleImages < Handler
-      URL = "https://ajax.googleapis.com/ajax/services/search/images"
-      VALID_SAFE_VALUES = %w(active moderate off)
+      # URL = "https://ajax.googleapis.com/ajax/services/search/images"
+      URL = "https://www.googleapis.com/customsearch/v1/"
+      VALID_SAFE_VALUES = %w(high medium off)
 
-      config :safe_search, types: [String, Symbol], default: :active do
+      config :safe_search, types: [String, Symbol], default: :high do
         validate do |value|
           unless VALID_SAFE_VALUES.include?(value.to_s.strip)
-            "valid values are :active, :moderate, or :off"
+            "valid values are :high, :medium, or :off"
           end
         end
       end
 
-      route(/(?:image|img)(?:\s+me)? (.+)/i, :fetch, command: true, help: {
+      config :cse_key
+      config :cse_cx
+
+      route(/(?:image|img)(?:\s+me)? (.+)/, :fetch, command: true, help: {
         "image QUERY" => "Displays a random image from Google Images matching the query."
       })
 
@@ -23,25 +27,30 @@ module Lita
 
         http_response = http.get(
           URL,
-          v: "1.0",
+          key: config.cse_key,
+          cx: config.cse_cx,
           q: query,
           safe: config.safe_search,
-          rsz: 8
+          num: 8,
+          start: rand(1..100),
+          imgSize: "medium",
+          imgType: "photo",
+          searchType: "image",
+          fileType: "jpg"
         )
 
         data = MultiJson.load(http_response.body)
 
-        if data["responseStatus"] == 200
-          choice = data["responseData"]["results"].sample
+        if http_response.status == 200
+          choice = data["items"].sample
           if choice
-            response.reply ensure_extension(choice["unescapedUrl"])
+            response.reply ensure_extension(choice["link"])
           else
             response.reply %{No images found for "#{query}".}
           end
         else
-          reason = data["responseDetails"] || "unknown error"
           Lita.logger.warn(
-            "Couldn't get image from Google: #{reason}"
+            "Couldn't get image from Google, sorry!"
           )
         end
       end
